750a0dd253a985343983e0facf472ffa458d4789
Chengkai <ck@qq.com>
2023-07-05 17:05:48 +0800

    push test

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("fourth commit!");
+        System.out.println("push test!");
     }
 }
